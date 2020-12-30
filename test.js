badc8e203e77269e16471434f9eb661ded5c602e
fisker <lionkay@gmail.com>
2020-12-30 16:09:05 +0800

    chore: update dependencies

--- test.js
+++ test.js
@@ -3,5 +3,5 @@ const fs = require('fs')
 const units = require('.')
 
 assert.strict.ok(Array.isArray(units), 'CSS units should be an array.')
-assert.strict.ok(units.length > 0, 'CSS units should not be empty.')
+assert.strict.ok(units.length !== 0, 'CSS units should not be empty.')
 assert.strict.ok(units.includes('px'), 'Git hooks should has `px`')
