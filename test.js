b656d558760f920bfd32ff9ee052136faf63ba43
fisker Cheung <lionkay@gmail.com>
2022-04-21 10:54:39 +0800

    Test with `node:test`

--- test.js
+++ test.js
@@ -1,15 +1,20 @@
 import assert from 'node:assert/strict'
+import test from 'node:test'
 import cssUnits from './index.js'
 import * as namedExports from './index.js'
 
-assert.ok(Array.isArray(cssUnits), 'CSS units should be an array.')
-assert.ok(cssUnits.length !== 0, 'CSS units should not be empty.')
-assert.ok(cssUnits.includes('px'), 'Git hooks should has `px`')
+test('Main', () => {
+  assert.ok(Array.isArray(cssUnits), 'CSS units should be an array.')
+  assert.ok(cssUnits.length !== 0, 'CSS units should not be empty.')
+  assert.ok(cssUnits.includes('px'), 'Git hooks should has `px`')
+})
 
 for (const [name, units] of Object.entries(namedExports)) {
-  assert.ok(Array.isArray(units), `'${name}' should be an array.`)
-  assert.ok(
-    units.every((unit) => cssUnits.includes(unit)),
-    `'${name}' should be a subset of all units.`,
-  )
+  test(`Named export '${name}'`, () => {
+    assert.ok(Array.isArray(units), `'${name}' should be an array.`)
+    assert.ok(
+      units.every((unit) => cssUnits.includes(unit)),
+      `'${name}' should be a subset of all units.`,
+    )
+  })
 }
