835cdc7ac522fcd065c70447240ec6b41dc05162
fisker <lionkay@gmail.com>
2026-07-25 15:17:40 +0800

    Add types

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+type Units = readonly string[]
+
+declare const allUnits: Units
+
+export default allUnits
+export const absoluteLengths: Units
+export const angleUnits: Units
+export const containerQueryLengthUnits: Units
+export const containerRelativeLengths: Units
+export const durationUnits: Units
+export const flexibleLengths: Units
+export const fontRelativeLengths: Units
+export const frequencyUnits: Units
+export const relativeLengths: Units
+export const resolutionUnits: Units
+export const viewportPercentageLengths: Units
